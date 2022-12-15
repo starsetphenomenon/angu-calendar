4517fee32a63ddf95117bf1b730d041ad09ac635
Пользователь <starsetphenomenon@gmail.com>
2022-12-15 11:44:56 +0200

    monorepo created

---
+++ apps/calendar/src/app/store/absence.actions.ts
@@ -0,0 +1,23 @@
+import { createAction, props } from '@ngrx/store';
+import { AbsenceItem } from '../components/calendar/calendar.component';
+import { AppState, AvailableDays } from './absence.reducer';
+
+export const addAbsence = createAction(
+  '[Absences] Add Absence',
+  props<AbsenceItem>()
+);
+
+export const deleteAbsence = createAction(
+  '[Absences] Delete Absence',
+  props<{ payload: number }>()
+);
+
+export const updateAbsence = createAction(
+  '[Absences] Update Absence',
+  props<{ absenceId: number; newAbsence: AbsenceItem }>()
+);
+
+export const setAvailableDays = createAction(
+  '[Available Days] Update Available Days',
+  props<AvailableDays>()
+);
---
+++ apps/calendar/src/app/store/absence.reducer.ts
@@ -0,0 +1,98 @@
+import { createReducer, on } from '@ngrx/store';
+import { AbsenceItem } from '../components/calendar/calendar.component';
+import {
+  addAbsence,
+  deleteAbsence,
+  setAvailableDays,
+  updateAbsence,
+} from './absence.actions';
+
+export interface AvailableDays {
+  sick: {
+    entitlement: number;
+    taken: number;
+  };
+  vacation: {
+    entitlement: number;
+    taken: number;
+  };
+}
+
+export interface Dialogs {
+  requestDialog: boolean;
+  updateDialog: boolean;
+}
+
+export interface AppState {
+  absences: AbsenceItem[];
+  availableDays: AvailableDays;
+}
+
+const initialState: AppState = {
+  availableDays: {
+    sick: {
+      entitlement: 20,
+      taken: 7,
+    },
+    vacation: {
+      entitlement: 10,
+      taken: 4,
+    },
+  },
+  absences: [
+    {
+      id: 1,
+      absenceType: 'sick',
+      fromDate: '2022-12-17',
+      toDate: '2022-12-21',
+      comment: 'I am SICK!',
+    },
+    {
+      id: 2,
+      absenceType: 'vacation',
+      fromDate: '2022-12-02',
+      toDate: '2022-12-07',
+      comment: 'Yuppi, freedom...',
+    },
+  ],
+};
+
+export const absenceReducer = createReducer(
+  initialState,
+  on(addAbsence, (state: AppState, action: AbsenceItem) => {
+    return {
+      ...state,
+      absences: [
+        ...state.absences,
+        { ...action, id: state.absences.length + 1 },
+      ],
+    };
+  }),
+  on(deleteAbsence, (state: AppState, action: { payload: number }) => {
+    return {
+      ...state,
+      absences: [...state.absences.filter((el) => el.id !== action.payload)],
+    };
+  }),
+  on(
+    updateAbsence,
+    (
+      state: AppState,
+      action: { absenceId: number; newAbsence: AbsenceItem }
+    ) => {
+      let newAbsences = state.absences.map((el) =>
+        el.id === action.absenceId ? action.newAbsence : el
+      );
+      return {
+        ...state,
+        absences: newAbsences,
+      };
+    }
+  ),
+  on(setAvailableDays, (state: AppState, action: AvailableDays) => {
+    return {
+      ...state,
+      availableDays: action,
+    };
+  })
+);
